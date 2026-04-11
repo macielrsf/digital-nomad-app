1e9ec1b6da60fb0c516e185b43dd3b28ad4af512
Maciel Rodrigues <maciel@Maciels-MacBook-Pro.local>
2026-04-11 16:24:29 -0300

    refactor(tests): Simplify Home screen integration test structure and remove timeout

--- src/__tests__/Home.integration.test.tsx
+++ src/__tests__/Home.integration.test.tsx
@@ -1,16 +1,30 @@
-import { screen } from '@testing-library/react-native';
+import {
+  fireEvent,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react-native';
 import { renderApp } from '../test-utils/renderApp';
 
-const TIMEOUT_FOR_DEBUG = 50000; // 50 seconds, to allow time for debugging if needed
 describe('integration: Home', () => {
-  it(
-    'should display the city list and navigate to details when the city card is pressed',
-    async () => {
-      //
-      renderApp({ isAuthenticated: true });
+  it('should display the city list and navigate to details when the city card is pressed', async () => {
+    renderApp({ isAuthenticated: true });
 
-      expect(await screen.findByText('Rio de Janeiro')).toBeOnTheScreen();
-    },
-    TIMEOUT_FOR_DEBUG
-  );
+    // Go to details screen
+    fireEvent.press(await screen.findByText('Rio de Janeiro'));
+
+    expect(await screen.findByText('Pontos turísticos')).toBeOnTheScreen();
+
+    // Go back to home screen
+    fireEvent.press(screen.getByTestId('Chevron-left'));
+
+    // Dubai city card
+    expect(await screen.findByText('Dubai')).toBeOnTheScreen();
+
+    fireEvent.changeText(screen.getByTestId('search-input'), 'barcelona');
+
+    await waitForElementToBeRemoved(() => screen.getByText('Dubai'));
+
+    expect(screen.getByText('Barcelona')).toBeOnTheScreen();
+    expect(screen.getByText('Espanha')).toBeOnTheScreen();
+  });
 });
