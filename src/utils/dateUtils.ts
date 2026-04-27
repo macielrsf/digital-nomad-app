25c9829338b727e38e1f89dab5fa4ff7d5c3d2d0
Maciel Rodrigues <maciel@Maciels-MacBook-Pro.local>
2026-04-27 08:01:29 -0300

    feat: add date-fns dependency and implement date formatting utility for user creation date

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,1 +1,12 @@
-// exemplo
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import { DateISO8601 } from '../domain/types';
+
+function formatMonthAndYear(date: DateISO8601): string {
+  return format(date, 'MMMM yyyy', { locale: ptBR });
+}
+
+export const dateUtils = {
+  formatMonthAndYear,
+};
