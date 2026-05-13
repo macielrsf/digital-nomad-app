5eccd91a7b97f6f489f5a31f184b822ffe01f833
Maciel Rodrigues <maciel@Maciels-MacBook-Pro.local>
2026-05-13 08:29:44 -0300

    feat: implement favorite cities feature with fetching and display in profile

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,6 +1,8 @@
 import { useAuthGetUser } from '@/src/domain/auth/operations/useAuthGetUser';
 import { useAuthSignOut } from '@/src/domain/auth/operations/useAuthSignOut';
+import { useFindAllFavorites } from '@/src/domain/city/operations/useFindAllFavorites';
 import { Box } from '@/src/ui/components/Box';
+import { CityCard } from '@/src/ui/components/CityCard';
 import { Icon } from '@/src/ui/components/Icon';
 import { Screen } from '@/src/ui/components/Screen';
 import { Text } from '@/src/ui/components/Text';
@@ -12,11 +14,19 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function ProfileScreen() {
   const { mutate: signOut } = useAuthSignOut();
   const { data: authUser } = useAuthGetUser();
+  const { data: favoriteList } = useFindAllFavorites();
 
   return (
     <Screen>
       <SafeAreaView>
         {authUser && <ProfileHeader authUser={authUser} />}
+
+        {favoriteList?.length > 0 ? (
+          favoriteList.map(city => <CityCard key={city.id} city={city} />)
+        ) : (
+          <Text>Nenhuma cidade favorita encontrada.</Text>
+        )}
+
         <Pressable onPress={signOut}>
           <Box
             mt='s24'
